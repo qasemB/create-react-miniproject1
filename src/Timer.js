dc42a6a321753f75bccb1aadc11c55db07b9dfdd
qasemB <mahdicmptr@gmail.com>
2021-10-14 13:33:06 +0330

    part #13

--- src/Timer.js
+++ src/Timer.js
@@ -9,35 +9,68 @@ class Timer extends React.Component{
   constructor(){
     super();
     this.state = {
-      time : 10
+      hour:0,
+      minute:0,
+      second:0,
+      isStart:false
     }
   }
 
-  componentDidMount(){
-    interval = setInterval(()=>{
+  startInterval = ()=>{
+    if (this.state.isStart == false) {
       this.setState({
-        time: this.state.time - 1
+        isStart: true
       })
-    } , 1000)
-  }
-
-  componentDidUpdate(){
-    if (this.state.time == 0) {
-      clearInterval(interval);
+      interval = setInterval(()=>{
+        this.setState({
+          second: this.state.second + 1
+        })
+        if (this.state.second == 60) {
+          this.setState({
+            second: 0,
+            minute: this.state.minute + 1
+          })
+        }
+        if (this.state.minute == 60) {
+          this.setState({
+            minute: 0,
+            hour: this.state.hour + 1
+          })
+        }
+      } , 1000)
     }
   }
 
-  componentWillUnmount(){
-    
+  stopInterval = ()=>{
+    this.setState({
+      isStart: false
+    })
+    clearInterval(interval);
+  }
+
+  resetInterval = ()=>{
+    this.stopInterval();
+    this.setState({
+      hour:0,
+      minute:0,
+      second:0,
+    })
   }
 
   render(){
+    let h = this.state.hour
+    let m = this.state.minute
+    let s = this.state.second
     return(
       <>
         <h2 className="timer">
-          it is {this.state.time}
+          {`${h > 9 ? h : "0"+h} : ${m > 9 ? m : "0"+m} : ${s > 9 ? s : "0"+s}`}
         </h2>
-        <button onClick={this.props.x}>change</button>
+        <div className="button_box">
+          <span className="action_button start_burtton" onClick={this.startInterval}>start</span>
+          <span className="action_button stop_burtton" onClick={this.stopInterval}>stop</span>
+          <span className="action_button reset_burtton" onClick={this.resetInterval}>reset</span>
+        </div>
       </>
     )
   }
