e5cd72bba2fec17ef99cd2140ba741aea49a14c6
qasemB <mahdicmptr@gmail.com>
2021-11-10 21:14:24 +0330

    part #23

--- src/TaskItems.jsx
+++ src/TaskItems.jsx
@@ -3,34 +3,38 @@ import { taskContext } from './taskContext';
 
 const TaskItems = ()=>{
     const {taskItems , setTaskItems} = useContext(taskContext);
-    return (
-        <ul className="list-group m-0 p-0 mt-2">
-            {
-                taskItems.map(t=>(
-                    <li 
-                    className={
-                        `list-group-item d-flex justify-content-between 
-                        ${t.done ? "list-group-item-success" : "" }`
-                        }>
-                       {t.title}
-                        <span>
-                            {
-                                t.done ? (
-                                    <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
-                                ) : (
-                                    <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
-                                )
-                            }  
-                            <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
-                        </span>
-                    </li>
-                ))
-            }
 
+    if (taskItems.length) {
+        return (
+            <ul className="list-group m-0 p-0 mt-2">
+                {
+                    taskItems.map(t=>(
+                        <li 
+                        className={
+                            `list-group-item d-flex justify-content-between 
+                            ${t.done ? "list-group-item-success" : "" }`
+                            }>
+                           {t.title}
+                            <span>
+                                {
+                                    t.done ? (
+                                        <i className="me-3 pointer fas fa-times text-warning transition_200 text_hover_shadow"></i>
+                                    ) : (
+                                        <i className="me-3 pointer fas fa-check text-success transition_200 text_hover_shadow"></i>
+                                    )
+                                }  
+                                <i className="me-3 pointer fas fa-trash text-danger transition_200 text_hover_shadow"></i>
+                            </span>
+                        </li>
+                    ))
+                }
+            </ul>    
+        )
+    }else{
+        return <h4 className="text-center text-danger mt-4">هیچ کاری ثبت نشده...!</h4>
+    }
 
-
-        </ul>    
-    )
+    
 }
 
 export default TaskItems;
--- src/TopForm.jsx
+++ src/TopForm.jsx
@@ -1,12 +1,26 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
+import { taskContext } from './taskContext';
 
 const TopForm = ()=>{
+    const [task , setTask] = useState("")
+
+    const {taskItems , setTaskItems} = useContext(taskContext)
+
+    const handleSetTask = (event)=>{
+        setTask(event.target.value)
+    }
+
+    const handleAddTask = (event)=>{
+        event.preventDefault()
+        setTaskItems([...taskItems , {id:Math.random() , title:task , done:false}])
+    }
+
     return(
         <>
             <h4 className="text-center text-info text_shdow">به کدیاد خوش اومدید</h4>
-            <form>
+            <form onSubmit={handleAddTask}>
                 <div className="form-group d-flex">
-                    <input type="text" className="form-control" />
+                    <input type="text" className="form-control" value={task} onChange={handleSetTask}/>
                     <button type="submit" className="btn btn-success me-1">ثبت</button>
                 </div>
             </form>
