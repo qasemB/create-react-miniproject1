af4cf29d9c18f3d5e3ae35103fa0f8fdf3b7a2ad
qasemB <mahdicmptr@gmail.com>
2021-12-18 23:14:23 +0330

    part #39

--- src/users/AddUser.jsx
+++ src/users/AddUser.jsx
@@ -1,13 +1,34 @@
-import React from 'react';
+import React , {useState} from 'react';
 import { useParams , Outlet, useNavigate, useLocation } from 'react-router';
 import style from '../style.module.css'
+import axios from 'axios'
 
 const AddUser = ()=>{
 
     const {userId} = useParams();
     const params = useLocation();
-
     const navigate = useNavigate();
+
+    const [data , setData] = useState({
+        name: "" ,
+        username : "",
+        email : "" ,
+        address : {
+            street: "",
+            city: "",
+            suite: "",
+            zipcode: ""
+        }
+    })
+
+    const handleAddUser = (e)=>{
+        e.preventDefault();
+        axios.post('https://jsonplaceholder.typicode.com/users' , data).then(res=>{
+            console.log(res);
+        });
+    }
+
+
 
     return (
         <div className={`${style.item_content} mt-5 p-4 container-fluid container`}>
@@ -15,32 +36,32 @@ const AddUser = ()=>{
                 {userId ? "ویرایش کاربر" : "افزودن کاربر" }
             </h4>
             <div className="row justify-content-center mt-5 ">
-                <form className="col-12 col-md-6 bg-light rounded shadow-lg p-3">
+                <form onSubmit={handleAddUser} className="col-12 col-md-6 bg-light rounded shadow-lg p-3">
                     <div className="mb-3">
                         <label className="form-label">نام و نام خانوادگی</label>
-                        <input type="text" className="form-control"/>
+                        <input type="text" className="form-control" value={data.name} onChange={(e)=>setData({...data , name:e.target.value})}/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">نام کاربری</label>
-                        <input type="text" className="form-control"/>
+                        <input type="text" className="form-control" value={data.username} onChange={(e)=>setData({...data , username:e.target.value})}/>
                     </div>
                     <div className="mb-3">
                         <label className="form-label">ایمیل</label>
-                        <input type="email" className="form-control"/>
+                        <input type="email" className="form-control" value={data.email} onChange={(e)=>setData({...data , email:e.target.value})}/>
                     </div>
                     <div className="mb-3 row">
                         <label className="form-label">آدرس</label>
                         <div className="col-6 my-1">
-                            <input type="text" className="form-control" placeholder="شهر"/>
+                            <input type="text" className="form-control" placeholder="شهر" value={data.address.city} onChange={(e)=>setData({...data , address:{...data.address , city:e.target.value}})}/>
                         </div>
                         <div className="col-6 my-1">
-                            <input type="text" className="form-control" placeholder="خیابان"/>
+                            <input type="text" className="form-control" placeholder="خیابان" value={data.address.street} onChange={(e)=>setData({...data , address:{...data.address , street:e.target.value}})}/>
                         </div>
                         <div className="col-6 my-1">
-                            <input type="text" className="form-control" placeholder="ادامه آدرس"/>
+                            <input type="text" className="form-control" placeholder="ادامه آدرس" value={data.address.suite} onChange={(e)=>setData({...data , address:{...data.address , suite:e.target.value}})}/>
                         </div>
                         <div className="col-6 my-1">
-                            <input type="text" className="form-control" placeholder="کد پستی"/>
+                            <input type="text" className="form-control" placeholder="کد پستی" value={data.address.zipcode} onChange={(e)=>setData({...data , address:{...data.address , zipcode:e.target.value}})}/>
                         </div>
                     </div>
                     
