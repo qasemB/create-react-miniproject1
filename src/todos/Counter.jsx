9daddf8f3c9d8e1460e104a59d101615174f9d4b
qasemB <mahdicmptr@gmail.com>
2022-01-06 11:57:08 +0330

    part #49

--- src/todos/Counter.jsx
+++ src/todos/Counter.jsx
@@ -1,12 +1,19 @@
 import React, { useReducer, useState } from 'react';
 
-const init = 0
+const init = {
+    value1 : 0,
+    value2 : 5
+}
 const reducer = (state , action)=>{
-    switch (action) {
+    switch (action.type) {
         case 'increment':
-            return state + 1
+            return {...state  , value1: state.value1 + action.val}
         case 'decrement':
-            return state - 1
+            return {...state  , value1: state.value1 - action.val}
+        case 'increment2':
+            return {...state  , value2: state.value2 + action.val}
+        case 'decrement2':
+            return {...state  , value2: state.value2 - action.val}
         case 'reset':
             return init
         default:
@@ -23,10 +30,20 @@ const Counter = () => {
 
     return (
         <div className="text-center my-3">
-            <h1 className='text-center'>{count}</h1>
-            <button className='btn btn-success' onClick={()=>dispatch('increment')} >افزایش</button>            
-            <button className='btn btn-danger' onClick={()=>dispatch('decrement')} >کاهش</button>            
-            <button className='btn btn-warning' onClick={()=>dispatch('reset')} >ریست</button>            
+            <h1 className='text-center'>{count.value1}</h1>
+            <h1 className='text-center'>{count.value2}</h1>
+            
+            <div className='text-center my-3'>
+                <button className='btn btn-success' onClick={()=>dispatch({type:'increment' , val: 1})} >افزایش</button>            
+                <button className='btn btn-danger' onClick={()=>dispatch({type: 'decrement' , val: 2})} >کاهش</button>  
+            </div>
+
+            <div className='text-center my-3'>
+                <button className='btn btn-success' onClick={()=>dispatch({type:'increment2' , val: 5})} >2افزایش</button>            
+                <button className='btn btn-danger' onClick={()=>dispatch({type: 'decrement2' , val: 5})} >2کاهش</button>  
+            </div>
+          
+            <button className='btn btn-warning' onClick={()=>dispatch({type:'reset'})} >ریست</button>            
         </div>
     );
 }
